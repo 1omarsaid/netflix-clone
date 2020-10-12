3b34e25ae1174f4c2ecf1eb1cc7cdd0b1f22d704
1omarsaid <omar.said@ryerson.ca>
2020-10-12 19:37:01 -0400

    added skeleton modal

--- src/Row.js
+++ src/Row.js
@@ -10,7 +10,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([])
     const [trailerUrl, setTrailerUrl] = useState("");
-
+    const [showM, setShowModal] = useState(false);
+    const [movie, setMovie] = useState([]);
+    // const movie = []
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -28,23 +30,55 @@ function Row({ title, fetchUrl, isLargeRow }) {
         }
     }
 
+    const Modal = ({ handleClose, show }) => {
+        const showHideClassName = show ? "modal display-block" : "modal display-none";
+        return (
+          <div className={showHideClassName}>
+            <section className="modal-main">
+              {movie.original_title}
+              <button onClick={handleClose}>close</button>
+            </section>
+          </div>
+        );
+      };
+
+    const showModal = () => {
+        setShowModal(true)
+    };
+    
+    const hideModal = () => {
+        setShowModal(false)
+    };
+
     const handleClick = (movie) => {
-        console.log(movie.name)
-        if(trailerUrl) {
-            setTrailerUrl('');
-        }
-        else {
-            movieTrailer(movie?.name || "")
-            .then(url => {
-                const urlParams = new URLSearchParams( new URL(url).search);
-                setTrailerUrl(urlParams.get('v'));
-            }).catch(err => console.log(err))
-        }
+        if(isLargeRow){
+            if(trailerUrl) {
+                setTrailerUrl('');
+            }
+            else {
+                movieTrailer(movie?.name || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams( new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                }).catch(err => {
+                    setMovie(movie)
+                    showModal()
+                    console.log(err)})
+            }
+        }else{
+            setMovie(movie)
+            showModal()
+        } 
+        
     }
     
     return (
         <div className="row">
             <h2>{title}</h2>
+            <Modal show={showM} handleClose={hideModal}>
+                <p>Modal</p>
+                <p>Data</p>
+            </Modal>
             <div className="row__posters">
                 {movies.map(movie => {
                     return movie.backdrop_path && (<img 
